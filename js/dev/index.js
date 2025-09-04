72371a3fe02b4381983ca1d6e539e8211a9e81f6
Гавриков Виталий Игоревич <gavrikov131313@gmail.com>
2025-09-05 00:23:18 +0300

    Updates

--- js/dev/index.js
+++ js/dev/index.js
@@ -5883,6 +5883,72 @@ function initSliders() {
       on: {}
     });
   }
+  if (document.querySelector(".type-services__slider")) {
+    new Swiper(".type-services__slider ", {
+      // <- Указываем класс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation, Pagination],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      //autoHeight: true,
+      speed: 800,
+      //touchRatio: 0,
+      //simulateTouch: false,
+      //loop: true,
+      //preloadImages: false,
+      //lazy: true,
+      /*
+      // Эффекты
+      effect: 'fade',
+      autoplay: {
+      	delay: 3000,
+      	disableOnInteraction: false,
+      },
+      */
+      // Пагинация
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      // Скроллбар
+      /*
+      scrollbar: {
+      	el: '.swiper-scrollbar',
+      	draggable: true,
+      },
+      */
+      // Кнопки "влево/вправо"
+      navigation: {
+        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next"
+      },
+      // Брейкпоинты
+      breakpoints: {
+        540: {
+          slidesPerView: 1.5,
+          spaceBetween: 20,
+          autoHeight: true
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        992: {
+          slidesPerView: 2.5,
+          spaceBetween: 20
+        },
+        1268: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+      },
+      // События
+      on: {}
+    });
+  }
 }
 document.querySelector("[data-fls-slider]") ? window.addEventListener("load", initSliders) : null;
 function showMore() {
