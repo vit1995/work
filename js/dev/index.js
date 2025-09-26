5466e14f976bace2c29322d9787b399bfbf02b23
Гавриков Виталий Игоревич <gavrikov131313@gmail.com>
2025-09-26 13:33:29 +0300

    Updates

--- js/dev/index.js
+++ js/dev/index.js
@@ -11690,18 +11690,3 @@ document.addEventListener("DOMContentLoaded", function() {
     hiddenHours.value = hoursValue;
   });
 });
-document.addEventListener("DOMContentLoaded", function() {
-  const navButton = document.querySelector(".nav");
-  const pageHeight = document.body.scrollHeight;
-  const triggerPoint = pageHeight * 0.5;
-  window.addEventListener("scroll", function() {
-    const scrollProgress = window.scrollY / triggerPoint;
-    if (scrollProgress >= 1) {
-      navButton.style.opacity = "1";
-      navButton.style.visibility = "visible";
-    } else {
-      navButton.style.opacity = "0";
-      navButton.style.visibility = "hidden";
-    }
-  });
-});
