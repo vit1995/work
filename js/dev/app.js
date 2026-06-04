590b94dac1020a5ae71bda58c446433593147408
vit1995 <111640998+vit1995@users.noreply.github.com>
2026-06-04 22:45:45 +0300

    Updates

--- js/dev/app.js
+++ js/dev/app.js
@@ -766,6 +766,7 @@ document.addEventListener("DOMContentLoaded", function() {
       if (window.innerWidth >= pcBreakpoint) {
         return;
       }
+      e.preventDefault();
       e.stopPropagation();
       console.log("Клик по меню:", this.textContent);
       menuItems.forEach((otherItem) => {
